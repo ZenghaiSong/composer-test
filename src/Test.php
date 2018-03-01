112f31df1ade1e54183b30b5a9d19e589e03716b
songzenghai <songzenghai@ylb.net>
2018-03-01 19:35:50 +0800

    test

---
+++ src/Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: songzh  <songzenghai@ylb.net>
+ * Date: 2018/3/1 下午7:33
+ * Desc:
+ */
+namespace szh\composerTest;
+
+class Test
+{
+    function __construct()
+    {
+        echo '__construct()'.PHP_EOL;
+    }
+
+    public function do($msg){
+        echo "hello world ".$msg.PHP_EOL;
+    }
+}
